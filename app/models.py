af052fe7a6e77bcf5a48743b11f9c3735f1083e6
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-12-15 11:59:28 -0600

    added the ImageField configuration

--- app/models.py
+++ app/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class ImageModel(models.Model):
-    name = models.CharField(max_length=25)
-    image = models.ImageField(upload_to='pictures', name=name)
+    image = models.ImageField(
+        upload_to='pictures', )
