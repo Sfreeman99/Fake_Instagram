04b17dab7eb740517c7e27ad83931cc1dc54498a
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2018-01-03 11:01:21 -0600

    Fixed bug with Model
    The model was not correctly uploading to the right directory but it is now

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,17 @@
 from django import forms
+from app.models import ImageModel
 
 
-class PostPhotoForm(forms.Form):
-    name = forms.CharField(max_length=15)
-    image = forms.ImageField(label='Upload Image')
+class PostPhotoForm(forms.ModelForm):
+    # name = forms.CharField(max_length=15)
+    # image = forms.ImageField(label='Upload Image')
+    class Meta:
+        model = ImageModel
+        fields = ('name', 'image')
+
+
+class EditPhotoForm(forms.Form):
+    over_lay = forms.ChoiceField(choices=[
+        ('/home/basecamp/Pictures/Happy_New_Year_FIlter.png', '2017 filter'),
+        ('None', 'No Filter'),
+    ])
--- app/models.py
+++ app/models.py
@@ -6,20 +6,11 @@ from django.db import models
 class ImageModel(models.Model):
     name = models.CharField(
         max_length=15, )
-    image = models.ImageField(
-        upload_to='app/static/pictures/', )
-    filtered = models.ImageField(
-        upload_to='app/static/filtered_pictures/', blank=True)
+    image = models.ImageField(upload_to='./app/static/pictures')
 
     def image_url(self):
         #return self.image.name[len('app/static/'):]
-        if self.filtered:
-            return self.filtered_url()
-        else:
-            return self.original_url()
+        return self.original_url()
 
     def original_url(self):
         return self.image.name[len('app/static/'):]
-
-    def filtered_url(self):
-        return self.filtered.name[len('app/static/')]
