25e2f1219d8c9f52f843440812e2b36001d0b226
Shedlia Freeman <shedliafreeman@Shedlias-MacBook-Pro.local>
2018-01-04 13:58:29 -0600

    Test was made for the model
    This test was made to see if the model would successfully run through with the requirements that i gave it. I

--- app/tests.py
+++ app/tests.py
@@ -1,4 +1,5 @@
 import pytest
+from app.models import ImageModel
 from app.forms import *
 
 
@@ -6,3 +7,12 @@ def test_Post_Photo_Form():
     form = PostPhotoForm({'name': ''})
     assert not form.is_valid()
     assert 'name' in form.errors
+
+
+@pytest.mark.django_db
+def test_Image_can_be_added():
+    new_image = ImageModel(name='Picture of me', image='foo.jpg')
+    new_image.save()
+    assert new_image.name == 'Picture of me'
+    assert new_image.image == 'foo.jpg'
+    assert ImageModel.objects.get(name='Picture of me')
