a0a847e1c617fce3d62022865ba301a372b62538
Shedlia Freeman <shedliafreeman@Shedlias-MacBook-Pro.local>
2018-01-05 13:27:57 -0600

    Added Test
    This test makes sure that my website has a button in the jumbotron that lets you post your photo. This test is currently failing

--- functional_tests.py
+++ functional_tests.py
@@ -10,17 +10,18 @@ class NewVisitor(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_if_website_will_load(self):
+    def test_if_website_will_have_the_title(self):
         # Shedlia goes to the website
         self.browser.get('http://localhost:8000/instagram/feed/')
         self.assertIn('Gram', self.browser.title)
 
-    def test_if_the_website_has_a_title(self):
+    def test_if_the_website_has_jumbotron(self):
         self.browser.get('http://localhost:8000/instagram/feed/')
         Jumbotron_text = self.browser.find_element_by_class_name(
             'jumbotron').text
         self.assertIn('Fake Instagram', Jumbotron_text, msg='No Jumbotron')
-        self.fail('Finish Test!')
+        self.assertIn('Post', Jumbotron_text, 'No post tab')
+        self.fail('Add link to <a> tag in jumbotron')
 
 
 if __name__ == '__main__':  #7
