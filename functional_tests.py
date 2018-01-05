16965abdd3ad70a03849c6c3c311386657827299
Shedlia Freeman <shedliafreeman@Shedlias-MacBook-Pro.local>
2018-01-05 13:00:43 -0600

    Added test to see if jumbotron is in code. Test is not yet passing

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,13 @@ class NewVisitor(unittest.TestCase):
         self.browser.get('http://localhost:8000/instagram/feed/')
         self.assertIn('Gram', self.browser.title)
 
+    def test_if_the_website_has_a_title(self):
+        self.browser.get('http://localhost:8000/instagram/feed/')
+        Jumbotron_text = self.browser.find_element_by_class_name(
+            'jumbotron').text
+        self.assertIn('Fake Instagram', Jumbotron_text, msg='No Jumbotron')
+        self.fail('Finish Test!')
+
 
 if __name__ == '__main__':  #7
     unittest.main(warnings='ignore')  #8
