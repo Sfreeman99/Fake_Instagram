5661055a25ce96ba73bdb32bd706d3781ebd4391
Shedlia Freeman <shedliafreeman@Shedlias-MBP.attlocal.net>
2018-01-10 23:17:08 -0600

    Test for Posting a photo
    I am adding a test that checks that when you go to the post_photo url it sends you to the right view. The test is currently not finished

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@ import unittest
 class NewVisitor(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(20)
 
     def tearDown(self):
         self.browser.quit()
@@ -21,7 +22,13 @@ class NewVisitor(unittest.TestCase):
             'jumbotron').text
         self.assertIn('Fake Instagram', Jumbotron_text, msg='No Jumbotron')
         self.assertIn('Post', Jumbotron_text, 'No post tab')
-        self.fail('Add link to <a> tag in jumbotron')
+
+    def test_if_you_can_successfully_go_to_the_post_form(self):
+        self.browser.get('http://localhost:8000/instagram/feed/')
+        link = self.browser.find_element_by_id('post_photo')
+        link.click()
+        assert self.current_url == 'http://localhost:8000/instagram/post_photo'
+        self.fail('Finish the test if this passes')
 
 
 if __name__ == '__main__':  #7
