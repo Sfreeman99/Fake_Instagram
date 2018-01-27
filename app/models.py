a26ef427866bfa7288b222f5f4c23113f316cfea
shedliafreeman <shedliafreeman@basecampcodingacademy.org>
2018-01-27 03:53:15 -0600

    Made a CommentModel which has a relationship with ImageModel

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class CommentModel(models.Model):
+    comment = models.CharField(max_length=250)
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ForeignKey("ImageModel", on_delete=models.CASCADE)
 
 
 class ImageModel(models.Model):
-    name = models.CharField(
-        max_length=15, )
+    name = models.CharField(max_length=15, )
     image = models.ImageField(upload_to='./app/static/pictures')
 
     def image_url(self):
